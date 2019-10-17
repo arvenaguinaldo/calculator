a99c1199b8eba5814e0c078312dd336102120066
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2019-10-18 02:12:14 +0800

    Updates

---
+++ precache-manifest.ec1e8888b3705c796488fcc471588017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1590c08e8bf27e55d56dd17d63ca8885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d12218eecb873a67254",
+    "url": "/static/css/main.474ad192.chunk.css"
+  },
+  {
+    "revision": "059a051805f2364f3698",
+    "url": "/static/js/2.ecfc3b2f.chunk.js"
+  },
+  {
+    "revision": "1d12218eecb873a67254",
+    "url": "/static/js/main.1a4bf0f2.chunk.js"
+  },
+  {
+    "revision": "d0096d1883e97c08ba09",
+    "url": "/static/js/runtime-main.2ab09cd8.js"
+  }
+]);
